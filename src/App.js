175213be5b9b3e35334db9a53292350a5e40d052
R-s-rawat <rohitrawat590590@gmail.com>
2024-09-01 20:48:53 +0530

    https://blog.logrocket.com/react-pagination-scratch-hooks/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Pagination from './Pagination';
 
 function App() {
+  
+   // STEP 1: a new  state variable called "currentPage", for USER 1st response
+   const [currentPage, setCurrentPage] = useState(1);
+
+   // STEP 1: useState hook (preserves values b/w function calls(takes only single @arg i.e initial state))
+
+   // STEP 2: useEffect hook - perform side effects, eg. call api
+
+   // GO TO PAGINATION.js for next steps followed STEP 3 - 9
+
+   // STEP 10: STEP 1: into action ;SET CURRENT PAGE to =1; and set DATA into ARRAY, & set UI PAGE limit 5,(for only 5 page numbers on UI)
+   const pageNumberLimit = 5;
+   const [passengersData, setData] = useState([])
+   const [loading, setLoading] = useState(true);
+  // const [currentPage, setCurrentPage] = useState(1);
+  const [maxPageLimit, setMaxPageLimit] = useState(5);
+  const [minPageLimit, setMinPageLimit] = useState(0);
+
+
+  // STEP 11: take STEP 2: into action;
+  useEffect(()=>{
+    setLoading(true);
+    fetch(`https://api.instantwebtools.net/v1/passenger?currentPage=${currentPage}&size=5`)
+    .then((response) => response.json())
+    .then((json) => { setData(json); setLoading(false);});
+
+  },[currentPage]);
+
+  // STEP 12: Finale
+  const onPageChange= (pageNumber)=>{
+    setCurrentPage(pageNumber);
+  }
+  const onPrevClick = ()=>{
+      if((currentPage-1) % pageNumberLimit === 0){
+          setMaxPageLimit(maxPageLimit - pageNumberLimit);
+          setMinPageLimit(minPageLimit - pageNumberLimit);
+      }
+      setCurrentPage(prev=> prev-1);
+   }
+
+  const onNextClick = ()=>{
+       if(currentPage+1 > maxPageLimit){
+           setMaxPageLimit(maxPageLimit + pageNumberLimit);
+           setMinPageLimit(minPageLimit + pageNumberLimit);
+       }
+       setCurrentPage(prev=>prev+1);
+    }
+
+    const paginationAttributes = {
+      currentPage,
+      maxPageLimit,
+      minPageLimit,
+      response: passengersData,
+    };
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+      <h2>Passenger List</h2>
+        {!loading ? <Pagination {...paginationAttributes} 
+                          onPrevClick={onPrevClick} 
+                          onNextClick={onNextClick}
+                          onPageChange={onPageChange}/>
+        : <div> Loading... </div>
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Pagination.js
@@ -0,0 +1,97 @@
+import React from 'react';
+// STEP 3: pagination reusable component; gets input from parent component e.g App.js
+
+
+// STEP 4: Initialize the variables for pagination logic
+
+const Pagination = (props) => {
+    const { currentPage, maxPageLimit, minPageLimit } = props;
+    const totalPages = props.response.totalPages - 1;
+    const data = props.response.data;
+
+    // STEP 5: build pages ARRAY, which will hold total/ALL page numbers
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+        pages.push(i);
+    }
+
+    const handlePrevClick = ()=>{
+        props.onPrevClick();
+    }
+    const handleNextClick = ()=>{
+        props.onNextClick();
+    }
+    const handlePageClick = (e)=>{
+        props.onPageChange(Number(e.target.id));
+    }
+
+      // STEP 6: Create UI for ALL page numbers b/w limits, using ARRAY(if iteration page matches current page, make it active)
+     const pageNumbers = pages.map(page=>{
+        if(page<= maxPageLimit && page>minPageLimit){
+            return(
+                <li key={page} id={page} onClick={handlePageClick} 
+                className={currentPage===page?'active':null}
+                >
+                    {page}
+                </li>
+            )
+        }
+     })
+
+     // STEP 7: Previous, Next buttons for pagination,-> page ellipses &hellip;
+     let pageIncrementEllipses = null;
+     if(pages.length > maxPageLimit){
+         pageIncrementEllipses = <li onClick={handleNextClick}>&hellip;</li>
+     }
+     let pageDecremenEllipses = null;
+     if(minPageLimit >=1){
+         pageDecremenEllipses = <li onClick={handlePrevClick}>&hellip;</li> 
+     }
+
+
+
+     // STEP 8: Render ---- API Response in list format (listing) 
+     const renderData = (data)=>{
+        return(
+            <ul>
+            {data.map((d)=> 
+            <li key={d['_id']}> The passenger having id {d['_id'].slice(d['_id'].length-5)} using {d.airline[0].name} airlines</li>)
+            }
+        </ul>
+        )
+     }
+
+     // STEP 9: COMBINE Logic and UI and return to parent (along with Prev and Next buttons)
+
+    return (
+        // Passenger DATA ---API
+        /*
+        Pager COMPONENT
+        1. Prev button
+        2. Page decrement ellipses
+        3. Page numbers with current page selection
+        4. Page increment ellipses
+        5. Next button
+        */
+         <div className='main'>
+            <div className='mainData'>
+                {/* // consume data */}
+                {renderData(data)}
+            </div>
+            {/* // page Numbers */}
+            <ul className='pageNumbers'>
+                <li>
+                    <button onClick={handlePrevClick} disabled={currentPage === pages[0]}>Prev</button>
+                </li>
+                {pageDecremenEllipses}
+                {pageNumbers}
+                {pageIncrementEllipses}
+                <li>
+                   <button onClick={handleNextClick} disabled={currentPage === pages[pages.length-1]}>&gt;Next</button>
+               </li>
+            </ul>
+            {/* //  END OF API CONSUMPTion */}
+         </div>
+    )
+}
+export default Pagination;
